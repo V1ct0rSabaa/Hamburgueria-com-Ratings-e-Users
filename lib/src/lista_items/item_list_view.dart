0a7da4a795235a5f0831ea274c3335a0c9b07b18
V1ct0rSabaa <victorsrurfx@gmail.com>
2024-05-06 00:09:37 -0300

    Sprint 1, mudanças esteticas apenas

--- lib/src/lista_items/item_list_view.dart
+++ lib/src/lista_items/item_list_view.dart
@@ -10,7 +10,7 @@ class ItemListView extends StatelessWidget {
     this.items = const [
       Item(1, "Hamburgueria Viking", 4.2, "*"), 
       Item(2, "Hamburgueria e Sushi Samurai", 2.1, "**"), 
-      Item(3, "Hamburgueria e Pizzaria Cavaleiro", 3.5, "*"),
+      Item(3, "Hamburgueria e Pizza Cavaleiro", 3.5, "*"),
       Item(4, "Hamburgueria Hussardo", 1.7, "****")
       ]
   });
@@ -33,25 +33,26 @@ class ItemListView extends StatelessWidget {
             },
           ),
         ],
+        backgroundColor: Color.fromARGB(255, 177,34,34),
       ),
-
       body: ListView.builder(
         restorationId: 'sampleItemListView',
         itemCount: items.length,
         itemBuilder: (BuildContext context, int index) {
           final item = items[index];
           const double maximoRating = 5.0;
-          final textoItem = """${item.nome}. Avaliação (${item.rating}/${maximoRating}). Valor Médio: ${item.valorMedio}""";
+          final textoItem = """${item.nome}. Avaliação (${item.rating} / $maximoRating). Valor Médio: ${item.valorMedio}""";
+          const Color corTexto = Color.fromARGB(255, 70,130,180);
           return ListTile(
-            title: Text(textoItem),
-            leading: const CircleAvatar(
-              // Display the Flutter Logo image asset.
-              foregroundImage: AssetImage('assets/images/hamb1.png'),
+            title: Text(textoItem, 
+            style: const TextStyle(color : corTexto), 
+
+            )
+            ,
+            leading: const CircleAvatar( // add icone circular de hamburguer ao lado de cada 
+              foregroundImage: AssetImage('assets/images/hamb1.png')
             ),
             onTap: () {
-              // Navigate to the details page. If the user leaves and returns to
-              // the app after it has been killed while running in the
-              // background, the navigation stack is restored.
               Navigator.restorablePushNamed(
                 context,
                 ItemDetailsView.routeName,
