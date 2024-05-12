9b9c0aff77f034423c61907837737a12ce1ca8d1
Felipe Machado Dias Ramos <felipemdramos@gmail.com>
2024-05-12 19:49:22 -0300

    Correção botão de detalhes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import '/src/views/item_details_view.dart';
+import '/src/views/item_list_view.dart';
 import 'screens/AuthScreen.dart';
 import 'settings/settings_controller.dart';
+import 'settings/settings_view.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key, required this.settingsController,});
@@ -29,23 +33,23 @@ class MyApp extends StatelessWidget {
           darkTheme: ThemeData.dark(),
           themeMode: settingsController.themeMode,
           home: const AuthScreen(),
-          // onGenerateRoute: (RouteSettings routeSettings) {
-          //   return MaterialPageRoute<void>(
-          //     settings: routeSettings,
-          //     builder: (BuildContext context) {
-          //       switch (routeSettings.name) {
-          //         case SettingsView.routeName:
-          //           return SettingsView(controller: settingsController);
-          //         case ItemDetailsView.routeName:
-          //           return const ItemDetailsView();
-          //         case ItemListView.routeName:
-          //           return const ItemListView();
-          //         default:
-          //            return const ItemListView();
-          //       }
-          //     },
-          //   );
-          // },
+           onGenerateRoute: (RouteSettings routeSettings) {
+             return MaterialPageRoute<void>(
+               settings: routeSettings,
+               builder: (BuildContext context) {
+                 switch (routeSettings.name) {
+                   case SettingsView.routeName:
+                     return SettingsView(controller: settingsController);
+                   case ItemDetailsView.routeName:
+                     return const ItemDetailsView();
+                   case ItemListView.routeName:
+                     return const ItemListView();
+                   default:
+                      return const ItemListView();
+                 }
+               },
+             );
+           },
         );
       },
     );
